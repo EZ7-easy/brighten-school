0b477f3cb97b7c73f6427e5bd026dd04e747cb48
EZ7-easy <b.sh.tutor@gmail.com>
2025-04-13 23:54:59 +0500

    fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,35 +1,36 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import { NextIntlClientProvider } from 'next-intl'
-import { getMessages } from 'next-intl/server'
-import React from 'react'
-import {Toaster} from "@/components/ui/sonner";
+import { unstable_setRequestLocale } from "next-intl/server";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { notFound } from "next/navigation";
 
-
-
-export const metadata: Metadata = {
-  title: 'hello',
+export const metadata = {
+  title: "My App",
 };
 
+export function generateStaticParams() {
+  return [{ locale: "en" }, { locale: "ru" }, { locale: "uz" }];
+}
 
-async function RootLayout({ children, params: {locale}
+export default async function LocaleLayout({
+  children,
+  params: { locale },
 }: {
   children: React.ReactNode;
-  params: {locale: string};
+  params: { locale: string };
 }) {
+  // Validate locale
+  if (!["en", "ru", "uz"].includes(locale)) notFound();
 
+  unstable_setRequestLocale(locale);
   const messages = await getMessages();
-  
+
   return (
     <html lang={locale}>
       <body>
         <NextIntlClientProvider messages={messages}>
           {children}
-          <Toaster position={'top-center'}/>
         </NextIntlClientProvider>
       </body>
     </html>
   );
 }
-
-export default RootLayout
