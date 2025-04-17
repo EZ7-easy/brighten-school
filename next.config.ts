405538f04dc5a172af1f657210bc6101c387e0db
EZ7-easy <b.sh.tutor@gmail.com>
2025-04-17 18:18:40 +0500

    turn off eslint

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true, // Disables ESLint during builds
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Disables TypeScript errors during builds
+  },
 };
 
 export default nextConfig;
